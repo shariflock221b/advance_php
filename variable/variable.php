b7a8070270273ac7cb35883aabd201629fdf897b
Shariflock <shariflocked@gmail.com>
2021-09-12 01:06:53 +0600

    Learn about variable
    variable declaration, variable rules etc

---
+++ variable/variable.php
@@ -0,0 +1,27 @@
+<?php 
+
+$num1 = 10;
+$num2 = 20;
+
+$sum_of_two = $num1 + $num2; 
+
+echo 'our result:  ' . $sum_of_two;
+
+echo '</br>';
+
+$bool = false;
+
+echo "our result: " . $bool;
+
+echo '</br>';
+
+$bool = true;
+
+echo "our result: { $bool}";
+
+
+echo '</br>';
+
+$bool = false;
+
+echo "our result: { $bool}";
