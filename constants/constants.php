6a91d03fb7e72e1155b6896b560bd5050eabb443
Shariflock <shariflocked@gmail.com>
2021-09-12 14:33:48 +0600

    Magic & predefine constants
    I learned . how to define magic & predefined php constants.

--- constants/constants.php
+++ constants/constants.php
@@ -11,5 +11,34 @@ echo num1;
 echo '</br>';
 
  $num = 10;
- $num = 20;
+ $num = 40;
 echo $num;
+
+
+
+//php magic constant
+
+
+
+echo '</br>';
+echo __FILE__;
+echo '</br>';
+// echo PHP_EOL;
+echo __LINE__;
+echo '</br>';
+echo PHP_VERSION;
+// echo PHP_EOL;
+echo '</br>';
+
+echo __DIR__;
+
+echo '</br>';
+echo __FUNCTION__;
+echo '</br>';
+echo __CLASS__;
+echo '</br>';
+echo __TRAIT__;
+echo '</br>';
+echo __METHOD__;
+echo '</br>';
+echo __NAMESPACE__;
