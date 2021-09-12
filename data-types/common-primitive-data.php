58a2c943bbe3699849a30c9da532841c49fad4ae
Shariflock <shariflocked@gmail.com>
2021-09-12 14:10:16 +0600

    Data Types..
    String
    Integer
    Float (floating point numbers - also called double)
    Boolean
    Array
    Object
    NULL

---
+++ data-types/common-primitive-data.php
@@ -0,0 +1,45 @@
+<?php
+$name = 'shariful islam';
+var_dump($name);
+
+echo '</br>';
+
+$num = 20345;
+
+var_dump($num);
+
+echo '</br>';
+
+$x = 10.20345;
+
+var_dump($x);
+
+echo '</br>';
+
+$bool = true;
+$bool_1 = false;
+
+var_dump($bool);
+
+echo '</br>';
+
+$fol = ['apple', 'orange', 'malta', 300];
+
+var_dump($fol);
+echo '</br>';
+echo $fol[1];
+echo '</br>';
+
+
+class car {
+
+}
+$car = new car();
+
+var_dump($car);
+
+
+echo '</br>';
+
+$null = null;
+var_dump($null);
